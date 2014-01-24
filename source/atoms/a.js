8fa3c2314028b49b13fe82a70eb0d50d99d7ef46
Richard Santos <richaardsantos@gmail.com>
2014-01-23 23:44:35 -0200

    #4 - External Links

--- source/atoms/a.js
+++ source/atoms/a.js
@@ -2,12 +2,14 @@
 *   <a> element - Test Case
 **/
 
-casper.test.begin("Anchor element", 3, function(test){
+casper.test.begin("Anchor element", 0, function(test){
 
     casper.start(nuclear.options.url, function() {
         test.assertDoesntExist("a[href='']", "Empty links");
         test.assertDoesntExist("a[href*='callto']", "Skype Deprecated Function");
         test.assertDoesntExist("a[href*='javascript']", "Behavior forbidden here");
+        var host = this.getCurrentUrl().split('/')[2];
+        test.assertDoesntExist("a[href^='http']:not([href*='" + host + "']):not([target='_blank'])", "External Links");
     })
 
     .then(function(){
