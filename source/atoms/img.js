699a4bd69cec08a464ec6dc345714393ea7bf55a
Richard Santos <richaardsantos@gmail.com>
2014-01-23 23:51:06 -0200

    #5

--- source/atoms/img.js
+++ source/atoms/img.js
@@ -2,10 +2,11 @@
 *   <img> element - Test Case
 **/
 
-casper.test.begin("Image element", 1, function(test){
+casper.test.begin("Image element", 0, function(test){
 
     casper.start(nuclear.options.url, function() {
         test.assertDoesntExist("img[src='']", "Empty Image");
+        test.assertDoesntExist("img:not([alt])", "Alternative Text");
     })
 
     .then(function(){
